6284be2016ec1dfd8fbd21113bcfaccdcc04784b
Brian Rono C. K. <bkcrono@gmail.com>
2026-02-03 13:39:28 +0100

    Phase 0: macOS prototype renders single coloring image

---
+++ ColoringPrototype/ColoringPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoringPrototypeApp.swift
+//  ColoringPrototype
+//
+//  Created by Brian C K on 03.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ColoringPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColoringPrototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColoringPrototype
+//
+//  Created by Brian C K on 03.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("drawing01")
+            .resizable()
+            .scaledToFit()
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
